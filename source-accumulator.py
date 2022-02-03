3509cda5ca30187c65c5a5719089c3c634d00d67
lminto <lminto@brave.com>
2022-02-03 16:09:18 +0000

    Changed scripts to be single run

--- source-accumulator.py
+++ source-accumulator.py
@@ -49,12 +49,10 @@ if os.path.isfile("output/sources.csv"):
         hash_titles = list(pd.read_csv("source_buckets/{}.csv".format(publisher_id), header=None).iloc[:, -1].to_numpy())
         checklist[publisher_id] = hash_titles
 
-while True:
-    print("Starting new parse...")
-    r = requests.get('https://brave-today-cdn.brave.com/brave-today/feed.json')
-    articles = r.json()
-    # TODO: in case script cannot hold state, PULL feed.csv,feed_buckets from S3
-    parse_articles_from_source(articles)
-    # TODO: PUSH source.csv,source_buckets changes to S3
-    print("Finished parsing articles from feed. Next parse in 3h hours.")
-    time.sleep(3*60*60)
+print("Starting new parse...")
+r = requests.get('https://brave-today-cdn.brave.com/brave-today/feed.json')
+articles = r.json()
+# TODO: PULL feed.csv,feed_buckets from S3
+parse_articles_from_source(articles)
+# TODO: PUSH source.csv,source_buckets changes to S3
+print("Finished parsing articles from feed. Next parse in 3h hours.")
--- source-similarity-matrix.py
+++ source-similarity-matrix.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import numpy as np
+from numpy import genfromtxtx
 import scipy
 import pandas as pd
 from tqdm import tqdm
@@ -56,3 +57,38 @@ for i in range(publisher_ids.size):
         sim_matrix[j,i] = sim
 
 np.savetxt("output/sim_matrix.csv", sim_matrix, delimiter=",")
+
+feeds = pd.read_csv("output/feeds.csv", header=None)
+feeds_ids = list(feeds.iloc[:,1].to_numpy())
+feeds_titles = list(feeds.iloc[:,0].to_numpy())
+
+sim_matrix = genfromtxt('output/sim_matrix.csv', delimiter=',')
+
+feed_dictionary = []
+for i, row in feeds.iterrows():
+    feed_dictionary.append({
+        'name':row[0],
+        'id': row[1]
+    })
+
+json_object = {'data':feed_dictionary}
+
+with open('output/sources.json', 'w', encoding='utf-8') as f:
+    json.dump(json_object, f, ensure_ascii=True, indent=4)
+
+top10_dictionary = {}
+for i, feed in enumerate(feeds_titles):
+    sources_ranking = []
+    for j in range(sim_matrix.shape[0]):
+        if i == j:
+            continue
+        sources_ranking.append((feeds_titles[j], sim_matrix[i, j]))
+
+    sources_ranking.sort(key=lambda x: -x[1])
+
+    top10_dictionary[feed] = [{'source':source[0], 'score':source[1]} for source in sources_ranking[:10]]
+
+with open('output/source_similarity_t10.json', 'w', encoding='utf-8') as f:
+    json.dump(top10_dictionary, f, ensure_ascii=True, indent=4)
+
+# PUSH source_similarity_t10, source.json and sim_matrix.csv
